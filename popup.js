debcbdeeec05af24fb1706a4ca3284f36d1be96e
LongHuy Nguyen <longhuy322000@gmail.com>
2018-12-29 12:05:10 -0500

    fixed add new class

--- popup.js
+++ popup.js
@@ -1,5 +1,5 @@
 var uid, itemNums = 0;
-var usersData;
+var usersData, clickedClass;
 
 function initApp() {
 
@@ -19,10 +19,7 @@ function initApp() {
             .onSnapshot(function(doc) {
               usersData = doc.data();
               var classList = document.getElementById('listClassMenu');
-              var first = false;
-              while (classList.firstChild) {
-                classList.removeChild(classList.firstChild);
-              }
+              clearElement('listClassMenu');
               for (var key in usersData)
               {
                 var li = document.createElement('li');
@@ -32,8 +29,7 @@ function initApp() {
                 button.id = key;
                 button.onclick = (function(key){
                   return function(){
-                    document.getElementById('addNewClassContents').style.display = "none";
-                    document.getElementById('classContents').style.display = "block";
+                    setContents('classContents');
                     classCLicked(key);
                   }
                 })(key);
@@ -47,7 +43,7 @@ function initApp() {
         {
           // can't find users in the database
           console.log("No such document!");
-          db.collection("users").doc(uid).set()
+          db.collection("users").doc(uid).set({});
         }
       }).catch(function(error) {
         console.log("Error getting document:", error);
@@ -102,162 +98,179 @@ window.onload = function() {
   initApp();
 };
 
+// clear all child elements in block
+function clearElement(element)
+{
+  var block = document.getElementById(element);
+  while (block.firstChild) {
+    block.removeChild(block.firstChild);
+  }
+}
+
 document.getElementById('logoutButton').onclick = function ()
 {
   firebase.auth().signOut();
 }
 
+function setContents(content)
+{
+  document.getElementById('addNewClassContents').style.display = "none";
+  document.getElementById('classContents').style.display = "none";
+  document.getElementById('edit-class-container').style.display = "none";
+  document.getElementById(content).style.display = "block";
+}
+
 // reset view
 function resetView()
 {
-  document.getElementById('addNewClassContents').style.display = "none";
-  document.getElementById('classContents').style.display = "block";
+  if (Object.keys(usersData).length === 0 && usersData.constructor === Object)
+  {
+    setContents('addNewClassContents');
+    return;
+  }
+
+  setContents('classContents');
 
   var first_element = Object.keys(usersData)[0];
   document.getElementById(first_element).style.backgroundColor = "#727272";
   document.getElementById(first_element).style.color = "white";
   classCLicked(first_element);
+  clickedClass = first_element;
+}
+
+function addEmptyItemToTable(tableName, choice)
+{
+  var table = document.getElementById(tableName);
+  var i = table.rows.length;
+  var tr = document.createElement('tr');
+  tr.id = "row" + choice + i;
+  tr.style.lineHeight = "0";
+  var thName = document.createElement('td');
+  var inputName = document.createElement('input');
+  inputName.value = "";
+  inputName.id = "itemName" + choice + i;
+  inputName.className = "form-control tableInput";
+  inputName.type = "text";
+  thName.append(inputName);
+
+  var thWeight = document.createElement('td');
+  var inputWeight = document.createElement('input');
+  inputWeight.value = "";
+  inputWeight.id = "weight" + choice + i;
+  inputWeight.className = "form-control tableInput";
+  inputWeight.type = "text";
+  thWeight.append(inputWeight);
+
+  var thGradeGot = document.createElement('td');
+  var numerator = document.createElement('input');
+  numerator.value = "";
+  numerator.id = "numerator" + choice + i;
+  numerator.className = "form-control numberTableInput";
+  numerator.style.width = "30px";
+  numerator.type = "number";
+  var divideLabel = document.createElement('p');
+  divideLabel.innerHTML = "/";
+  divideLabel.style.display = "inline-block";
+  var denumerator = document.createElement('input');
+  denumerator.value = "";
+  denumerator.id = "denumerator" + choice + i;
+  denumerator.className = "form-control numberTableInput";
+  denumerator.style.width = "30px";;
+  denumerator.style.padding = "0";
+  denumerator.type = "number";
+  thGradeGot.append(numerator);
+  thGradeGot.append(divideLabel);
+  thGradeGot.append(denumerator);
+
+  var thGradeNeed = document.createElement('td');
+  thGradeNeed.style.textAlign = "center";
+  var img = document.createElement('img');
+  img.src = "images/delete.svg";
+  img.className = "deleteImg";
+  img.onclick = (function(i) {
+    return function(){
+      document.getElementById('row' + choice + i).style.display = "none";
+    }
+  })(i);
+  thGradeNeed.append(img);
+
+  tr.append(thName);
+  tr.append(thWeight);
+  tr.append(thGradeGot);
+  tr.append(thGradeNeed);
+  table.append(tr);
 }
 
 // Allow users to add new Class contents
 document.getElementById('addClassButton').onclick = function ()
 {
   itemNums = 0;
-  document.getElementById('weightInfo').style.display = "none";
-  document.getElementById('newClassButtonContainers').style.display = "none";
   for (var name in usersData)
   {
     document.getElementById(name).style.backgroundColor = "transparent";
     document.getElementById(name).style.color = "black";
   }
-  document.getElementById('addNewClassContents').style.display = "block";
-  document.getElementById('classContents').style.display = "none";
-  document.getElementById('weightInfo').style.display = "none";
-
-  var div = document.getElementById('items-new-class');
-  while (div.firstChild) {
-    div.removeChild(div.firstChild);
-  }
-  document.getElementById('className').value = "";
-  document.getElementById('classNameWarning').style.display = "none";
-  document.getElementById('newClassWarning').style.display = "none";
-}
-
-// Add new items to the new class
-document.getElementById('addNewItem').onclick = function ()
-{
-  itemNums++;
-  var div = document.getElementById('items-new-class');
-  var label = document.createElement('label');
-  label.innerHTML = itemNums + ". Item:";
-
-  var name = document.createElement('input');
-  name.id = "itemName" + itemNums;
-  name.placeholder = "Name";
-  name.className = "form-control new-item";
-  name.type = "text";
-  name.style.height = "25px";
-  name.style.fontSize = "13px";
-  name.style.marginLeft = "25px";
-  name.style.width = "125px";
-
-  var weightLabel = document.createElement('label');
-  weightLabel.innerHTML = "Weight: ";
-  weightLabel.style.marginLeft = "13px";
-
-  var weight = document.createElement('input');
-  weight.id = "weight" + itemNums;
-  weight.placeholder = "Only number";
-  weight.className = "form-control new-item";
-  weight.type = "number";
-  weight.style.width = "125px";
-  weight.style.height = "25px";
-  weight.style.fontSize = "13px";
-
-  var gradeLabel = document.createElement('label');
-  gradeLabel.innerHTML = "Grade: ";
-  gradeLabel.style.marginLeft = "5px";
-
-  var numerator = document.createElement('input');
-  numerator.id = "numerator" + itemNums;
-  numerator.className = "form-control new-item";
-  numerator.type = "number";
-  numerator.style.width = "60px";
-  numerator.style.height = "25px";
-  numerator.style.fontSize = "13px";
-  numerator.style.paddingRight = "0";
-  var divideLabel = document.createElement('label');
-  divideLabel.innerHTML = "/";
-  divideLabel.style.marginLeft = "5px";
-  var denumerator = document.createElement('input');
-  denumerator.id = "denumerator" + itemNums;
-  denumerator.className = "form-control new-item";
-  denumerator.type = "number";
-  denumerator.style.width = "60px";
-  denumerator.style.height = "25px";
-  denumerator.style.fontSize = "13px";
-  denumerator.style.paddingRight = "0";
-  denumerator.style.marginLeft = "5px";
-
-  div.append(label);
-  div.append(name);
-  div.append(document.createElement('br'));
-  div.append(weightLabel);
-  div.append(weight);
-  div.append(gradeLabel);
-  div.append(numerator);
-  div.append(divideLabel);
-  div.append(denumerator);
-  div.append(document.createElement('br'));
-
-  if (itemNums == 1)
-  {
-    document.getElementById('newClassButtonContainers').style.display = "block";
-    document.getElementById('weightInfo').style.display = "block";
-  }
+  setContents('addNewClassContents');
+  clearElement('addClassBodyContents');
+  document.getElementById('classNameAdd').value = "";
+  document.getElementById('targetScoreAdd').value = "";
+  document.getElementById('classNameAddWarning').style.display = "none";
+  document.getElementById('targetScoreAddWarning').style.display = "none";
+  document.getElementById('newClassAddWarning').style.display = "none";
+  addEmptyItemToTable('addClassBodyContents', "Add");
 }
 
 // Submit the new class to the database
 document.getElementById('submitNewClass').onclick = function ()
 {
   // print warning if class name is empty
-  if (document.getElementById('className').value == '')
+  if (document.getElementById('classNameAdd').value == '')
   {
-    document.getElementById('classNameWarning').style.display = "block";
+    document.getElementById('classNameAddWarning').style.display = "block";
     return;
   }
   else
-    document.getElementById('classNameWarning').style.display = "none";
+    document.getElementById('classNameAddWarning').style.display = "none";
 
-  // print warning if item name is empty
-  for (var i = 1; i<=itemNums; i++)
+  if (document.getElementById('targetScoreAdd').value == '')
   {
-    if (document.getElementById('itemName' + i).value == '' || document.getElementById('weight' + i).value == ''
-        || document.getElementById('numerator' + i).value == '' || document.getElementById('denumerator' + i).value == '')
+    document.getElementById('targetScoreAddWarning').style.display = "block";
+    return;
+  }
+  else
+    document.getElementById('targetScoreAddWarning').style.display = "none";
+
+  // print warning if some fields are empty
+  var table = document.getElementById('addClassBodyContents');
+  for (var i = 0, row; row = table.rows[i]; i++)
+  {
+    if (row.style.display != "none")
     {
-      document.getElementById('newClassWarning').style.display = "block";
-      return;
+      if (document.getElementById('itemNameAdd' + i).value == "" || document.getElementById('weightAdd' + i).value == "")
+      {
+        document.getElementById('newClassAddWarning').style.display = "block";
+        return;
+      }
     }
   }
-  document.getElementById('newClassWarning').style.display = "none";
+  document.getElementById('newClassAddWarning').style.display = "none";
 
   // get data
   var data = [];
-  for (var i = 1; i<=itemNums; i++)
+  for (var i = 0, row; row = table.rows[i]; i++)
   {
-    var dict = {};
-    dict[document.getElementById('itemName' + i).value] = {
-      weight: document.getElementById('weight' + i).value,
-      numerator: document.getElementById('numerator' + i).value,
-      denumerator: document.getElementById('denumerator' + i).value
-    };
-    data.push(dict);
+    data.push({
+      name: document.getElementById('itemNameAdd' + i).value,
+      weight: document.getElementById('weightAdd' + i).value,
+      numerator: document.getElementById('numeratorAdd' + i).value,
+      denumerator: document.getElementById('denumeratorAdd' + i).value
+    });
   }
-  data['total_score'] = 0;
+  data.push({target_score: document.getElementById('targetScoreAdd').value});
 
   var updatedData = {};
-  updatedData[document.getElementById('className').value] = data;
-  console.log(updatedData);
+  updatedData[document.getElementById('classNameAdd').value] = data;
+  console.log(updatedData)
   db.collection("users").doc(uid).update(updatedData);
   resetView();
 }
@@ -271,6 +284,7 @@ document.getElementById('cancelNewClass').onclick = function ()
 // User click on class
 function classCLicked(key)
 {
+  clickedClass = key;
   for (var name in usersData)
   {
     document.getElementById(name).style.backgroundColor = "transparent";
@@ -279,25 +293,191 @@ function classCLicked(key)
   document.getElementById(key).style.backgroundColor = "#727272";
   document.getElementById(key).style.color = "white";
 
-  var table = document.getElementById('classsBodyContents');
-  while (table.firstChild) {
-    table.removeChild(table.firstChild);
-  }
-  for (var i in usersData[key])
+  clearElement('classBodyContents');
+  var table = document.getElementById('classBodyContents');
+
+  var size = usersData[key].length;
+  var weightNeed = 0;
+  var weightGot = 0;
+  var targetScore = parseInt(usersData[key][size-1]['target_score']);
+  var sumScore = 0;
+  for (var i=0; i< size-1; i++)
   {
-    var item = Object.keys(usersData[key][i])[0];
-    if (item == "total_score")
-      continue;
+    if (usersData[key][i]['numerator'] == '' || usersData[key][i]['denumerator'] == '')
+    {
+      weightNeed += parseInt(usersData[key][i]['weight']);
+    }
+    else
+    {
+      weightGot += parseInt(usersData[key][i]['weight']);
+      sumScore += parseInt(usersData[key][i]['numerator']) / parseInt(usersData[key][i]['denumerator']) * parseInt(usersData[key][i]['weight']);
+    }
+  }
+  console.log({
+    weightGot: weightGot,
+    weightNeed: weightNeed,
+    sumScore: sumScore,
+    targetScore: targetScore
+  })
+  targetScore = (weightGot + weightNeed) / 100 * targetScore;
+  targetScore = (targetScore - sumScore) / weightNeed;
+  console.log(targetScore);
+
+  for (var i=0; i< size-1; i++)
+  {
     var tr = document.createElement('tr');
-    var thName = document.createElement('th');
-    thName.innerHTML = item;
-    var thWeight = document.createElement('th');
-    thWeight.innerHTML = usersData[key][i][item]['weight'];
-    var thGrade = document.createElement('th');
-    thGrade.innerHTML = usersData[key][i][item]['numerator'] + " / " + usersData[key][i][item]['denumerator'];
+    var thName = document.createElement('td');
+    thName.innerHTML = usersData[key][i]['name'];
+    var thWeight = document.createElement('td');
+    thWeight.innerHTML = usersData[key][i]['weight'];
+    var thGradeGot = document.createElement('td');
+    var thGradeNeed = document.createElement('td');
+    if (usersData[key][i]['numerator'] == '' || usersData[key][i]['denumerator'] == '')
+    {
+      thGradeGot.innerHTML = "";
+      thGradeNeed.innerHTML = (targetScore * 100).toFixed(2) + "%";
+    }
+    else
+    {
+      thGradeGot.innerHTML = usersData[key][i]['numerator'] + " / " + usersData[key][i]['denumerator'];
+      thGradeNeed.innerHTML = "";
+    }
     tr.append(thName);
     tr.append(thWeight);
-    tr.append(thGrade);
+    tr.append(thGradeGot);
+    tr.append(thGradeNeed);
+    table.append(tr);
+  }
+
+  document.getElementById('targetScoreText').innerHTML = usersData[key][size-1]['target_score'] + "%";
+  document.getElementById('currentScore').innerHTML = (sumScore / weightGot * 100).toFixed(2) + "%";
+}
+
+// Allow users to edit class contents
+document.getElementById('editClassButton').onclick = function()
+{
+  var key =  clickedClass;
+  var size = usersData[key].length;
+  setContents('edit-class-container');
+  document.getElementById('classNameChange').value = key;
+  document.getElementById('targetScoreChange').value = usersData[key][size-1]['target_score'];
+
+  clearElement('editClassContents');
+  var table = document.getElementById('editClassContents');
+
+  for (var i=0; i< size-1; i++)
+  {
+    var tr = document.createElement('tr');
+    tr.id = "rowChange" + i;
+    tr.style.lineHeight = "0";
+    var thName = document.createElement('td');
+    var inputName = document.createElement('input');
+    inputName.value = usersData[key][i]['name'];
+    inputName.id = "itemNameChange" + i;
+    inputName.className = "form-control tableInput";
+    inputName.type = "text";
+    thName.append(inputName);
+
+    var thWeight = document.createElement('td');
+    var inputWeight = document.createElement('input');
+    inputWeight.value = usersData[key][i]['weight'];
+    inputWeight.id = "weightChange" + i;
+    inputWeight.className = "form-control tableInput";
+    inputWeight.type = "text";
+    thWeight.append(inputWeight);
+
+    var thGradeGot = document.createElement('td');
+    var numerator = document.createElement('input');
+    numerator.value = usersData[key][i]['numerator'];
+    numerator.id = "numeratorChange" + i;
+    numerator.className = "form-control numberTableInput";
+    numerator.style.width = "30px";
+    numerator.type = "number";
+    var divideLabel = document.createElement('p');
+    divideLabel.innerHTML = "/";
+    divideLabel.style.display = "inline-block";
+    var denumerator = document.createElement('input');
+    denumerator.value = usersData[key][i]['denumerator'];
+    denumerator.id = "denumeratorChange" + i;
+    denumerator.className = "form-control numberTableInput";
+    denumerator.style.width = "30px";;
+    denumerator.style.padding = "0";
+    denumerator.type = "number";
+    thGradeGot.append(numerator);
+    thGradeGot.append(divideLabel);
+    thGradeGot.append(denumerator);
+
+    var thGradeNeed = document.createElement('td');
+    thGradeNeed.style.textAlign = "center";
+    var img = document.createElement('img');
+    img.src = "images/delete.svg";
+    img.className = "deleteImg";
+    img.onclick = (function(i) {
+      return function(){
+        document.getElementById('rowChange' + i).style.display = "none";
+      }
+    })(i);
+    thGradeNeed.append(img);
+
+    tr.append(thName);
+    tr.append(thWeight);
+    tr.append(thGradeGot);
+    tr.append(thGradeNeed);
     table.append(tr);
   }
 }
+
+// delete a class
+document.getElementById('deleteClassButton').onclick = function()
+{
+  var deleteData = {};
+  deleteData[clickedClass] = firebase.firestore.FieldValue.delete();
+  db.collection("users").doc(uid).update(deleteData);
+}
+
+// save changes for editing
+document.getElementById('saveChangesButton').onclick = function()
+{
+  var key = clickedClass;
+  var table = document.getElementById('editClassContents');
+  var data = [];
+
+  for (var i = 0, row; row = table.rows[i]; i++)
+  {
+    if (row.style.display != "none")
+    {
+      data.push({
+        name: document.getElementById('itemNameChange' + i).value,
+        weight: document.getElementById('weightChange' + i).value,
+        numerator: document.getElementById('numeratorChange' + i).value,
+        denumerator: document.getElementById('denumeratorChange' + i).value
+      });
+    }
+  }
+  data.push({target_score: document.getElementById('targetScoreChange').value});
+  var updatedData = {};
+  updatedData[document.getElementById('classNameChange').value] = data;
+  var deleteData = {};
+  deleteData[key] = firebase.firestore.FieldValue.delete();
+
+  db.collection("users").doc(uid).update(deleteData);
+  db.collection("users").doc(uid).update(updatedData);
+}
+
+// cancel edit class
+document.getElementById('cancelChangesButton').onclick = function()
+{
+  resetView();
+}
+
+// add new item to edit class table
+document.getElementById('addItemChanges').onclick = function()
+{
+  addEmptyItemToTable('editClassContents', "Change");
+}
+
+document.getElementById('addItemAddClass').onclick = function()
+{
+  addEmptyItemToTable('addClassBodyContents', "Add");
+}
+
